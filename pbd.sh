65e9c2a3c59aff7a5272279d08d63cbf7bf08793
alfie-ns <alfienurse@gmail.com>
2024-06-20 04:37:05 +0100

    update

---
+++ pbd.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#!/bin/bash
+
+# Get the current directory name
+current_dir=$(basename "$PWD")
+
+# 1. Run the push script
+if ./push.sh; then # if push.sh succeeds then...
+
+  # 2. Change to the parent directory
+  cd ..
+
+  # 3. Remove the repository directory
+  rm -rf "$current_dir"
+else # if push.sh fails
+  echo ""
+  echo "--------------------------------"
+  echo "Error: push.sh failed. Exiting. |"
+  echo "--------------------------------"
+  echo ""
+fi
+
+# 'alfie-ns' ascii
+cat <<'EOF'
+
+ ⚙️ Process complete ⚙️ 
+----------------------
+         _  __ _                     
+   __ _ | |/ _(_) ___       _ __  ___
+  / _` || | |_| |/ _ \_____| '_ \/ __|
+ | (_| || |  _| |  __/_____| | | \__ \
+  \__,_||_|_| |_|\___|     |_| |_|___/
+  
+EOF
